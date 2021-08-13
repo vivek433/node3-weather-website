1cd34b7a7f52da4da05a04adf6e0f3003767c01d
vivek kumar pandey <vp444625@gmail.com>
2021-08-13 11:52:51 +0530

    Setup app  for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ e.preventDefault()
 const location=search.value
 messageOne.textContent='Loading...'
 messageTwo.textContent=''
-fetch("http://localhost:3000/weather?address="+location).then((response)=>
+fetch("/weather?address="+location).then((response)=>
 {
 response.json().then((data)=>
 {console.log(data)
--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const hbs=require('hbs')
 const geocode=require('./utils/geocode')
 const forecast=require('./utils/forecast')
 const app=express()
+const port=process.env.PORT || 3000
 //Define path for express config
 const publicDirectoryPath=path.join(__dirname,'../public')
 const viewsPath=path.join(__dirname,'../templates/views')
@@ -81,7 +82,7 @@ app.get('*',(req,res)=>
        })
 })
 
-app.listen(3000,()=>
+app.listen(port,()=>
 {
-    console.log("server is up!")
+    console.log("server is up on"+ port)
 })
